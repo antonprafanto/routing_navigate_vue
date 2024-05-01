818468381c305cf4d1a88cc5bf1bb4f4e54fb0e4
antonprafanto <anton.prafanto@gmail.com>
2024-05-01 13:29:21 +0800

    kedua

---
+++ src/components/Awal.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="awal">
+        <p>Halaman <b>Awal</b></p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HalamanAwal'
+}
+</script>
---
+++ src/components/Tentang.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="tentang">
+        <p>Halaman <b>Tentang</b></p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HalamanTentang'
+}
+</script>
